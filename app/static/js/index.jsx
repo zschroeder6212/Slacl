a159b0b8b2f2cd7ea083bcb8ca17831a3b376377
zschroeder6212@gmail.com <zschroeder6212@gmail.com>
2021-07-08 07:03:46 -0500

    started nav layout

--- app/static/js/index.jsx
+++ app/static/js/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from "./components/navbar/navbar.jsx"
 
 import "./index.css"
 import "./normalize.css"
@@ -7,3 +8,7 @@ import "./normalize.css"
  * TODO: write app
  */
 
+ ReactDOM.render(
+     <NavBar />,
+     document.getElementById('react-entrypoint')
+ )
